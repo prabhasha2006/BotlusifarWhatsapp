0295eda837f7fdcd1bbba0f49c199ea10086f0ba
kavishka <91202334+kavishkaya@users.noreply.github.com>
2021-11-26 18:35:48 +0530

    ERROR FIX

--- plugins/Lusifarupdfg.js
+++ plugins/Lusifarupdfg.js
@@ -1,7 +1,7 @@
 /* Copyright (C) 2021 KAVISHKA SANDARUWAN 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Lusifar bot
+Lusifar bots
 */
 
 const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
