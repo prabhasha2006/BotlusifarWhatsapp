e9bdd52c900e134e5f31562ca9d1bf018198efab
kavishka <91202334+kavishkaya@users.noreply.github.com>
2021-11-26 17:43:55 +0530

    Added picture For Update command

--- plugins/updater.js
+++ plugins/updater.js
@@ -1,14 +1,14 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2021 kavishka
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+lusifar - kavishka
 */
 
 const simpleGit = require('simple-git');
 const git = simpleGit();
-const MyPnky = require('../events');
+const lusifar = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 const exec = require('child_process').exec;
@@ -19,53 +19,41 @@ const heroku = new Heroku({ token: Config.HEROKU.API_KEY })
 const Language = require('../language');
 const Lang = Language.getString('updater');
 
+//උස්සන උන්ගෙ රෙපෝ කෙලවිලාම පලයම්
+var logoimage = new Array ();
 
+logoimage[0] = "https://telegra.ph/file/863a715abb69894732eaf.jpg";
+logoimage[1] = "https://telegra.ph/file/863a715abb69894732eaf.jpg";
 
-MyPnky.addCommand({pattern: 'update$', fromMe: true, dontAddCommandList: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
+  var i = Math.floor(2*Math.random())
+
+  var uplogonew = logoimage[i]    
+
+lusifar.addCommand({pattern: 'update$', fromMe: true, dontAddCommandList: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
     await git.fetch();
     var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
     if (commits.total === 0) {
-        await message.client.sendMessage(
-            message.jid,
-            Lang.UPDATE, MessageType.text
-        );    
+        
+        var webimage = await axios.get(`${uplogonew}`, { responseType: 'arraybuffer' })
+        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg  , caption: Lang.UPDATE +'\n\n\n\n\n\n\n\n\n\n *⚡powerd by lusifar*' })
+    
     } else {
-        var degisiklikler = Lang.NEW_UPDATE;
+        var newzels = Lang.NEW_UPDATE;
         commits['all'].map(
             (commit) => {
-                degisiklikler += '🔹 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' <' + commit.author_name + '>\n';
+                newzels += '🔹 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' ◁◁' + commit.author_name + '▷▷\n';
             }
         );
         
-        await message.client.sendMessage(
-            message.jid,
-            degisiklikler + '```', MessageType.text
-        ); 
+        var webimage = await axios.get(`${uplogonew}`, { responseType: 'arraybuffer' })
+        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg  , caption: newzels + '```'+'\n\n *⚡powerd by lusifar*' })
+        
     }
-}));
-
-
-MyPnky.addCommand({pattern: 'updts$', fromMe: true, dontAddCommandList: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
-    await git.fetch();
-    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
-    if (commits.total === 0) {
-        await message.client.sendMessage(message.jid,Lang.UPDATE, MessageType.text
-        );    
-    } else {
-        var degisiklikler = Lang.NEW_UPDATE;
-        commits['all'].map(
-            (commit) => {
-                degisiklikler += '🔹 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' <' + commit.author_name + '>\n';
-            }
-        );
-        var image = await axios.get ('https://telegra.ph/file/e8f3e419b3dafe9fe8153.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: degisiklikler + '```' })
-    }
-}));
+ }));  
 
 
 
-MyPnky.addCommand({pattern: 'update now$', fromMe: true,dontAddCommandList: true, desc: Lang.UPDATE_NOW_DESC}, (async (message, match) => {
+lusifar.addCommand({pattern: 'update now$', fromMe: true,dontAddCommandList: true, desc: Lang.UPDATE_NOW_DESC}, (async (message, match) => {
     await git.fetch();
     var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
     if (commits.total === 0) {
