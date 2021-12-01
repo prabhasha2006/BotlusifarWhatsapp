ba19a3f6f32b3d91b0afb7605d3e8acf970bd650
kavishka <91202334+kavishkaya@users.noreply.github.com>
2021-12-01 19:00:29 +0530

    song fix

--- plugins/song.js
+++ plugins/song.js
@@ -2,7 +2,7 @@
 /* 🆕Copyright (C) 2020  kavishka sandaruwan - lusifar
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-kavishka sandaruwan - lusifar*/
+kavishka sandaruwan - lusifar
 
 const lusifar = require('../events');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
@@ -115,16 +115,7 @@ const Clang = Language.getString('covid');
                 await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: false, quoted: message.data});
             });
     }));
-
-
-
-
-
-/* 🆕Copyright (C) 2020  kavishka sandaruwan - lusifar
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-kavishka sandaruwan - lusifar
-
+*/
 
 
 const lusifar = require('../events');
@@ -137,13 +128,8 @@ const NO_RESULT = "*🌀can't Find Anything...*"
 const config = require('../config');
 let KSK = config.WORKTYPE == 'public' ? false : true
 //උස්සන්නද ආවෙ බේසිකයෝ.බොහොම අමාරුවෙන් හැදුවෙ.උස්සන එකා අවජාතකයෙක් කියල හිතාගන්න පුලුවන් පොන්න හැත්ත.හුකන පොන්නයෝ
-
-
 //normal mode
-
 //with info
-
-
     lusifar.addCommand({ pattern: 'song ?(.*)', fromMe: KSK, deleteCommand: false, dontAddCommandList:true,  deleteCommand: false}, async (message, match) => {
         const linkk = match[1]
         if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
@@ -152,20 +138,21 @@ let KSK = config.WORKTYPE == 'public' ? false : true
           .get(`https://api-alphabot.herokuapp.com/api/downloader/youtube/playmp3?query=${linkk}&apikey=Alphabot`)
           .then(async (response) => {
             const {
-              mp3,
+              mp3,title,
             } = response.data.results
             const videoBuffer = await axios.get(mp3, {responseType: 'arraybuffer'})
+            const cptt = title
             await message.client.sendMessage(message.jid,config.SONGU,MessageType.text, {quoted: message.data});
-            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.audio, {quoted: message.data ,mimetype: Mimetype.mp4audio, ptt: false})
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.audio, { mimetype: 'audio/mpeg', quoted: message.data})
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.document, {filename: cptt + '.mp3',mimetype: 'audio/mpeg', quoted: message.data})
         })
         .catch(
           async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
         )
       },
     )
-
-
-    lusifar.addCommand({ pattern: 'song ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList:true,  deleteCommand: false}, async (message, match) => {
+    
+        lusifar.addCommand({ pattern: 'song ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList:true,  deleteCommand: false}, async (message, match) => {
         const linkk = match[1]
         if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
         await message.client.sendMessage(message.jid,config.SONGD,MessageType.text, {quoted: message.data});
@@ -173,15 +160,16 @@ let KSK = config.WORKTYPE == 'public' ? false : true
           .get(`https://api-alphabot.herokuapp.com/api/downloader/youtube/playmp3?query=${linkk}&apikey=Alphabot`)
           .then(async (response) => {
             const {
-              mp3,
+              mp3,title,
             } = response.data.results
             const videoBuffer = await axios.get(mp3, {responseType: 'arraybuffer'})
+            const cptt = title
             await message.client.sendMessage(message.jid,config.SONGU,MessageType.text, {quoted: message.data});
-            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.audio, {quoted: message.data ,mimetype: Mimetype.mp4audio, ptt: false})
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.audio, {mimetype: 'audio/mpeg', quoted: message.data})
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.document, {filename: cptt + '.mp3',mimetype: 'audio/mpeg', quoted: message.data})
         })
         .catch(
           async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
         )
       },
-    )*/
-
+    )
