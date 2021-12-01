f18cf1a36a2ed6f0385389cde791a2feb4fe8bfa
kavishka <88529228+lkruwan@users.noreply.github.com>
2021-12-01 19:59:45 +0530

    FIX

--- plugins/_lusifar.js
+++ plugins/_lusifar.js
@@ -17,7 +17,7 @@ const lusifarv = fs.readFileSync('./uploads/voicenew/lusifarv.mp3')
 let WORKN = Config.WORKTYPE == 'public' ? false : true
 let WORKM = Config.WORKTYPE == 'public' ? true : true
 
-    lusifar.addCommand({pattern: '${Config.PANEL_COMMAND} ?(.*)', fromMe: WORKN, dontAddCommandList: true}, (async (message, match) => {
+    lusifar.addCommand({pattern: Config.PANEL_COMMAND + '(.*)', fromMe: WORKN, dontAddCommandList: true}, (async (message, match) => {
 
         var CMD_HELP = '';
         if (match[1] === '') {
@@ -229,7 +229,7 @@ let WORKM = Config.WORKTYPE == 'public' ? true : true
             );
         }
     }));
-   lusifar.addCommand({pattern: '${Config.PANEL_COMMAND} ?(.*)', fromMe: WORKM, dontAddCommandList: true}, (async (message, match) => {
+   lusifar.addCommand({pattern: Config.PANEL_COMMAND + '(.*)', fromMe: WORKM, dontAddCommandList: true}, (async (message, match) => {
 
         var CMD_HELP = '';
         if (match[1] === '') {
