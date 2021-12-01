b440fa5ea33356607f951d652ffdb13db702a112
kavishka <88529228+lkruwan@users.noreply.github.com>
2021-12-01 10:19:17 +0530

    song downloader fix

--- plugins/song.js
+++ plugins/song.js
@@ -3,7 +3,6 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 kavishka sandaruwan - lusifar
-*/
 
 const lusifar = require('../events');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
@@ -116,3 +115,73 @@ const Clang = Language.getString('covid');
                 await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: false, quoted: message.data});
             });
     }));
+*/
+
+
+
+
+/* 🆕Copyright (C) 2020  kavishka sandaruwan - lusifar
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+kavishka sandaruwan - lusifar
+*/
+
+
+const lusifar = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const { errorMessage, infoMessage } = require('../helpers');
+const YTV_DESC = "Youtube song Downloader "
+const YT_NEED = "*need word!.*"
+const NO_RESULT = "*🌀can't Find Anything...*"
+const config = require('../config');
+let KSK = config.WORKTYPE == 'public' ? false : true
+//උස්සන්නද ආවෙ බේසිකයෝ.බොහොම අමාරුවෙන් හැදුවෙ.උස්සන එකා අවජාතකයෙක් කියල හිතාගන්න පුලුවන් පොන්න හැත්ත.හුකන පොන්නයෝ
+
+
+//normal mode
+
+//with info
+
+
+    lusifar.addCommand({ pattern: 'song ?(.*)', fromMe: KSK, deleteCommand: false, dontAddCommandList:true,  deleteCommand: false}, async (message, match) => {
+        const linkk = match[1]
+        if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
+        await message.client.sendMessage(message.jid,config.SONGD,MessageType.text, {quoted: message.data});
+        await axios
+          .get(`https://api-alphabot.herokuapp.com/api/downloader/youtube/playmp3?query=${linkk}&apikey=Alphabot`)
+          .then(async (response) => {
+            const {
+              mp3,
+            } = response.data.results
+            const videoBuffer = await axios.get(mp3, {responseType: 'arraybuffer'})
+            await message.client.sendMessage(message.jid,config.SONGU,MessageType.text, {quoted: message.data});
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.audio, {quoted: message.data ,mimetype: Mimetype.mp4audio, ptt: false})
+        })
+        .catch(
+          async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
+        )
+      },
+    )
+
+
+    lusifar.addCommand({ pattern: 'song ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList:true,  deleteCommand: false}, async (message, match) => {
+        const linkk = match[1]
+        if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
+        await message.client.sendMessage(message.jid,config.SONGD,MessageType.text, {quoted: message.data});
+        await axios
+          .get(`https://api-alphabot.herokuapp.com/api/downloader/youtube/playmp3?query=${linkk}&apikey=Alphabot`)
+          .then(async (response) => {
+            const {
+              mp3,
+            } = response.data.results
+            const videoBuffer = await axios.get(mp3, {responseType: 'arraybuffer'})
+            await message.client.sendMessage(message.jid,config.SONGU,MessageType.text, {quoted: message.data});
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.audio, {quoted: message.data ,mimetype: Mimetype.mp4audio, ptt: false})
+        })
+        .catch(
+          async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
+        )
+      },
+    )
+
