fe6c44ac654d22d224c66b990e3c0f17c8961383
kavishka <88529228+lkruwan@users.noreply.github.com>
2021-12-09 17:48:48 +0530

    FB FIX

--- plugins/1tfb.js
+++ plugins/1tfb.js
@@ -1,96 +1,25 @@
 
-/* Copyright (C) 2021 lusifar
 
-
-
+/* Copyright (C) 2021 lusifar */
 // අමාරුවෙන් යකො හැදුවෙ. උස්සන්න එපා තිරිසනෝ 😒
-
 const lusifar = require('../events');
 const {MessageType,mimetype,Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
-const Language = require('../language');
-const Lang = Language.getString('scrapers');
+const Config = require('../config');
 const capt = "　🧛‍♂️ *LUSIFAR FB DOWNLOADER* 💜\n"
 const DOWNMSG = "🧛‍♂️ *DOWNLOADING FB VIDEO...* 🤍"
 const UPMSG = "🧛‍♂️ _*U P L O A D I N G*_  💕"
-let KSK = Alexa.WORKTYPE == 'public' ? false : true
-let teenuh = Alexa.WORKTYPE == 'public' ? true : false
-const Config = require('../config');
-                           config.js
-/*
-Download.Facebook{
-	addmeta : 100000;
-		{
-      "is_eligible_for_download": true,
-      "downloable_id": "1353269864728879_1943344825721377",
-      "id": "1353269864728879_1943344825721377"
-    },
-	video.info
-	{
-      "created_time": "{date}T16:24:04+0000",
-      "message": "Request Data",
-      "id": "{page-id}_2191966997525824"
-    },
-	
-	FB.login(function(response) {
-    if (response.authResponse) {
-     console.log('Welcome!  Fetching your information.... ');
-     FB.api('/me', function(response) {
-       console.log('Good to see you, ' + response.name + '.');
-     });
-    } else {
-     console.log('User cancelled login or did not fully authorize.');
-    }
-});
-	x-business-use-case-usage: {api-type == get 
-    "{business-object-id}": [
-        {
-            "type": "{rate-limit-type}",          
-            "call_count": 100,                 
-            "total_cputime": 25,                   
-            "total_time": 25,                      
-            "estimated_time_to_regain_access": 19  
-        }
-    ],      
-    "66782684": [
-        {
-            "type": "ads_management",
-            "call_count": 95,
-            "total_cputime": 20,
-            "total_time": 20,
-            "estimated_time_to_regain_access": 0
-        }
-    ],
-    "10153848260347724": [
-        {
-            "type": "ads_management",
-            "call_count": 97,
-            "total_cputime": 23,
-            "total_time": 23,
-            "estimated_time_to_regain_access": 0
-        }
-    ],
-...
-}
-{ if error {   
-"error": {      
-    "message": "(#80001) There have been too many calls to this Page account. Wait a bit and try again. For more info, please refer to https://developers.facebook.com/docs/Config-api/overview/rate-limiting.",      
-    "type": "OAuthException",      
-    "code": 80001,      
-    "fbtrace_id": "AmFGcW_3hwDB7qFbl_QdebZ"   
-    }
-}}
-} 
+let KSKK = Config.WORKTYPE == 'public' ? true : true
+let KSP = Config.WORKTYPE == 'public' ? true : false
 
+                           
 // -----------------------------------------------------------------------
 //                               MAIN COMMAND
 // -----------------------------------------------------------------------
 lusifar.addCommand({ pattern: 'fb ?(.*)', fromMe: KSP , desc: 'facebook video download'}, async (message, match) => {
 // අමාරුවෙන් යකො හැදුවෙ. උස්සන්න එපා තිරිසනෝ 😒
     const link = match[1]
-
     if (!link) return await message.sendMessage("🧛‍♂️ *Give Vaild FB Link* ")
-
     await message.sendMessage(DOWNMSG)
 	
 	var url = `${Config.SITE1}socialmedia/${Config.FBS2}=${link}&apikey=${Config.BAPIME}`
@@ -101,11 +30,9 @@ lusifar.addCommand({ pattern: 'fb ?(.*)', fromMe: KSP , desc: 'facebook video do
 						const {
 							hd
 						} = response.data.result
-
 						const kavifb = await axios.get(hd, {responseType: 'arraybuffer'})
 						await new Promise(r => setTimeout(r, 2000));
 						await message.client.sendMessage(message.jid,UPMSG,MessageType.text);
-
 						await message.sendMessage(Buffer.from(kavifb.data), MessageType.video, {caption: capt,})
 							.catch(
 								async(err) => await message.sendMessage("⛔️ *INVALID FB LINK*"),
@@ -113,13 +40,10 @@ lusifar.addCommand({ pattern: 'fb ?(.*)', fromMe: KSP , desc: 'facebook video do
 					})
 					
 }) //pattern close
-
 lusifar.addCommand({ pattern: 'fb ?(.*)', fromMe: KSKK , dontAddCommandList: true}, async (message, match) => {
 // අමාරුවෙන් යකො හැදුවෙ. උස්සන්න එපා තිරිසනෝ 😒
     const link = match[1]
-
     if (!link) return await message.sendMessage("🧛‍♂️ *Give Vaild FB Link* ")
-
     await message.sendMessage(DOWNMSG)
 	
 	var url = `${Config.SITE1}socialmedia/${Config.FBS2}=${link}&apikey=${Config.BAPIME}`
@@ -130,11 +54,9 @@ lusifar.addCommand({ pattern: 'fb ?(.*)', fromMe: KSKK , dontAddCommandList: tru
 						const {
 							hd
 						} = response.data.result
-
 						const kavifb = await axios.get(hd, {responseType: 'arraybuffer'})
 						await new Promise(r => setTimeout(r, 2000));
 						await message.client.sendMessage(message.jid,UPMSG,MessageType.text);
-
 						await message.sendMessage(Buffer.from(kavifb.data), MessageType.video, {caption: capt,})
 							.catch(
 								async(err) => await message.sendMessage("⛔️ *INVALID FB LINK*"),
@@ -143,17 +65,13 @@ lusifar.addCommand({ pattern: 'fb ?(.*)', fromMe: KSKK , dontAddCommandList: tru
 					
 }) //pattern close
  
-
 // -----------------------------------------------------------------------
 //                               NOT WORKING COMMAND
 // -----------------------------------------------------------------------
-
 lusifar.addCommand({ pattern: 'xxfbsd ?(.*)', fromMe:KSKK , dontAddCommandList: true}, async (message, match) => {
 // අමාරුවෙන් යකො හැදුවෙ. උස්සන්න එපා තිරිසනෝ 😒
     const link = match[1]
-
     if (!link) return await message.sendMessage("🧛‍♂️ *Give Vaild FB Link* ")
-
     await message.sendMessage('⛔️ _*ERROR 404*_')
 	
 	var url = `${Config.SITE1}socialmedia/${Config.FBS2}=${link}&apikey=${Config.BAPIME}`
@@ -164,11 +82,9 @@ lusifar.addCommand({ pattern: 'xxfbsd ?(.*)', fromMe:KSKK , dontAddCommandList: 
 						const {
 							sd
 						} = response.data.result
-
 						const kavifb = await axios.get(sd, {responseType: 'arraybuffer'})
 						await new Promise(r => setTimeout(r, 2000));
 						await message.client.sendMessage(message.jid,UPMSG,MessageType.text);
-
 						await message.sendMessage(Buffer.from(kavifb.data), MessageType.video, {caption: capt,})
 							.catch(
 								async(err) => await message.sendMessage("⛔️ *INVALID FB LINK*"),
@@ -176,191 +92,3 @@ lusifar.addCommand({ pattern: 'xxfbsd ?(.*)', fromMe:KSKK , dontAddCommandList: 
 					})
 					
 }) //pattern close
-*/
-
-
-
-
-/* Copyright (C) 2021 RAMIYA-ALEXA TEAM.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-ALEXA TEAM - RAMIYA
-
-// අමාරුවෙන් යකො හැදුවෙ. උස්සන්න එපා තිරිසනෝ 😒
-
-const Lusifar = require('../events');
-const {MessageType,mimetype,Mimetype} = require('@adiwajshing/baileys');
-const axios = require('axios');
-var ruq = { cd: 'Li4vZmlsZXMvcmFtaXlh', encrypt: 'Code Encrypted BY RAMIYA' }
-var code = Buffer.from(ruq.cd, 'base64')
-var file = code.toString('utf-8')
-const Alexa = require('../config');
-const Language = require('../language');
-const Lang = Language.getString('scrapers');
-const ALang = Language.getString('amazone');
-const capt = "　🧛‍♂️ *LUSIFAR FB DOWNLOADER* 💜\n"
-const DOWNMSG = "🧛‍♂️ *DOWNLOADING FB VIDEO...* 🤍"
-const UPMSG = "🧛‍♂️ _*U P L O A D I N G*_  💕"
-let ramiya = Alexa.WORKTYPE == 'public' ? false : true
-let teenuh = Alexa.WORKTYPE == 'public' ? true : false
-
-/*
-Download.Facebook{
-	addmeta : 100000;
-		{
-      "is_eligible_for_download": true,
-      "downloable_id": "1353269864728879_1943344825721377",
-      "id": "1353269864728879_1943344825721377"
-    },
-	video.info
-	{
-      "created_time": "{date}T16:24:04+0000",
-      "message": "Request Data",
-      "id": "{page-id}_2191966997525824"
-    },
-	
-	FB.login(function(response) {
-    if (response.authResponse) {
-     console.log('Welcome!  Fetching your information.... ');
-     FB.api('/me', function(response) {
-       console.log('Good to see you, ' + response.name + '.');
-     });
-    } else {
-     console.log('User cancelled login or did not fully authorize.');
-    }
-});
-	x-business-use-case-usage: {api-type == get 
-    "{business-object-id}": [
-        {
-            "type": "{rate-limit-type}",          
-            "call_count": 100,                 
-            "total_cputime": 25,                   
-            "total_time": 25,                      
-            "estimated_time_to_regain_access": 19  
-        }
-    ],      
-    "66782684": [
-        {
-            "type": "ads_management",
-            "call_count": 95,
-            "total_cputime": 20,
-            "total_time": 20,
-            "estimated_time_to_regain_access": 0
-        }
-    ],
-    "10153848260347724": [
-        {
-            "type": "ads_management",
-            "call_count": 97,
-            "total_cputime": 23,
-            "total_time": 23,
-            "estimated_time_to_regain_access": 0
-        }
-    ],
-...
-}
-{ if error {   
-"error": {      
-    "message": "(#80001) There have been too many calls to this Page account. Wait a bit and try again. For more info, please refer to https://developers.facebook.com/docs/alexa-api/overview/rate-limiting.",      
-    "type": "OAuthException",      
-    "code": 80001,      
-    "fbtrace_id": "AmFGcW_3hwDB7qFbl_QdebZ"   
-    }
-}}
-} 
-
-// -----------------------------------------------------------------------
-//                               MAIN COMMAND
-// -----------------------------------------------------------------------
-Lusifar.addCommand({ pattern: 'fb ?(.*)', fromMe: ramiya , desc: ALang.FBDOWN}, async (message, match) => {
-// අමාරුවෙන් යකො හැදුවෙ. උස්සන්න එපා තිරිසනෝ 😒
-    const link = match[1]
-
-    if (!link) return await message.sendMessage("🧛‍♂️ *Give Vaild FB Link* ")
-
-    await message.sendMessage(DOWNMSG)
-	
-	var url = `${Alexa.SITE1}socialmedia/${Alexa.FBS2}=${link}&apikey=${Alexa.BAPIME}`
-				// 
-				await axios 
-					.get(`${url}`)
-					.then(async(response) => {
-						const {
-							hd
-						} = response.data.result
-
-						const alexafb = await axios.get(hd, {responseType: 'arraybuffer'})
-						await new Promise(r => setTimeout(r, 2000));
-						await message.client.sendMessage(message.jid,UPMSG,MessageType.text);
-
-						await message.sendMessage(Buffer.from(alexafb.data), MessageType.video, {caption: capt,})
-							.catch(
-								async(err) => await message.sendMessage("⛔️ *INVALID FB LINK*"),
-							)
-					})
-					
-}) //pattern close
-
-Lusifar.addCommand({ pattern: 'fb ?(.*)', fromMe: teenuh , desc: ALang.FBDOWN}, async (message, match) => {
-// අමාරුවෙන් යකො හැදුවෙ. උස්සන්න එපා තිරිසනෝ 😒
-    const link = match[1]
-
-    if (!link) return await message.sendMessage("🧛‍♂️ *Give Vaild FB Link* ")
-
-    await message.sendMessage(DOWNMSG)
-	
-	var url = `${Alexa.SITE1}socialmedia/${Alexa.FBS2}=${link}&apikey=${Alexa.BAPIME}`
-				// 
-				await axios 
-					.get(`${url}`)
-					.then(async(response) => {
-						const {
-							hd
-						} = response.data.result
-
-						const alexafb = await axios.get(hd, {responseType: 'arraybuffer'})
-						await new Promise(r => setTimeout(r, 2000));
-						await message.client.sendMessage(message.jid,UPMSG,MessageType.text);
-
-						await message.sendMessage(Buffer.from(alexafb.data), MessageType.video, {caption: capt,})
-							.catch(
-								async(err) => await message.sendMessage("⛔️ *INVALID FB LINK*"),
-							)
-					})
-					
-}) //pattern close
- 
-
-// -----------------------------------------------------------------------
-//                               NOT WORKING COMMAND
-// -----------------------------------------------------------------------
-
-Lusifar.addCommand({ pattern: 'xxfbsd ?(.*)', fromMe: ramiya , dontAddCommandList: true}, async (message, match) => {
-// අමාරුවෙන් යකො හැදුවෙ. උස්සන්න එපා තිරිසනෝ 😒
-    const link = match[1]
-
-    if (!link) return await message.sendMessage("🧛‍♂️ *Give Vaild FB Link* ")
-
-    await message.sendMessage('⛔️ _*ERROR 404*_')
-	
-	var url = `${Alexa.SITE1}socialmedia/${Alexa.FBS2}=${link}&apikey=${Alexa.BAPIME}`
-				// 
-				await axios 
-					.get(`${url}`)
-					.then(async(response) => {
-						const {
-							sd
-						} = response.data.result
-
-						const alexafb = await axios.get(sd, {responseType: 'arraybuffer'})
-						await new Promise(r => setTimeout(r, 2000));
-						await message.client.sendMessage(message.jid,UPMSG,MessageType.text);
-
-						await message.sendMessage(Buffer.from(alexafb.data), MessageType.video, {caption: capt,})
-							.catch(
-								async(err) => await message.sendMessage("⛔️ *INVALID FB LINK*"),
-							)
-					})
-					
-}) //pattern close
-*/
