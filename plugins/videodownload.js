dbf8265266543bbef52c01e697edfb51e30ddafc
kavishka <88529228+lkruwan@users.noreply.github.com>
2021-12-17 19:40:53 +0530

    SHORT DOWNLOADER FIX

--- plugins/videodownload.js
+++ plugins/videodownload.js
@@ -176,148 +176,117 @@ if (config.YT_INFO == 'false') {
 
 
 
+
+
+
+
 const lusifar = require('../events');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
 const ytdl = require('ytdl-core');
 const YTV_DESC = "Youtube Video Downloader "
 const YT_NEED = "*need word!.*"
 const DWLOAD_VID = "*🎭Downloading Your Video...*"
-const YTV_UP = "*🚀Uploading Your short Video...*"
-const DSWLOAD_VID = "*🎭Downloading Your short Video...*"
+const YTV_UP = "*🚀Uploading Your Video...*"
+const DSWLOAD_VID = "*🎭Downloading Your Video...*"
 const YSTV_UP = "*🚀Uploading Your Video...*"
 const NO_RESULT = "*🌀can't Find Anything...*"
 const fs = require('fs');
 const axios = require('axios');
 const config = require('../config');
-let  PUBH = config.WORKTYPE == 'public' ? true : true
-let KSK = config.WORKTYPE == 'public' ? false : true
+let tk = config.WORKTYPE == 'public' ? false : true
+let PUBFM = config.WORKTYPE == 'public' ? true : true
 
 
-//public mode
-    lusifar.addCommand({pattern: 'video ?(.*)', fromMe: KSK, desc: YTV_DESC}, (async (message, match) => { 
+lusifar.addCommand({
+	            pattern: 'video ?(.*)', 
+		    fromMe: tk, 
+		    desc: YTV_DESC
+		                    }, (async (message, match) => { 
 
-        if (match[1] === '') return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text , {quoted: message.data});    
-        if (match[1].includes( 'shorts' )){
-        var VID = '';
-        try {
-            if (match[1].includes('watch')) {
+await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text , {quoted: message.data});   
+    
+    var VID = '';
+    try {
+            if (match[1].includes( 'shorts' )){
+            var rmx = match[1].replace( 'shorts/', '')
+            var rmy = rmx.replace( '?feature=share','')
+            var data = rmy.split( '/' )[3]
+            VID = data
+								
+         } else
+         
+         
+                 if (match[1].includes('watch')) {
                 var tsts = match[1].replace('watch?v=', '')
                 var alal = tsts.split('/')[3]
                 VID = alal
             } 
-            
-if (match[1].includes( 'shorts' )) {
-                var rmx = match[1].replace( 'shorts/', '')
-				var rmy = rmx.replace( '?feature=share','')
-                var data = rmy.split( '/' )[3]
-                VID = data
-            
-             }
-            
-            else {     
-                VID = match[1].split('/')[3]
+         
+       else {     
+				 VID = match[1].split('/')[3]
             }
         } catch {
-            return await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text , {quoted: message.data});
+            return await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text);
         }
-        var reply = await message.client.sendMessage(message.jid,DSWLOAD_VID,MessageType.text , {quoted: message.data});
-
-        var yt = ytdl(VID, {filter: format => format.container === 'mp4' && ['720p', '480p', '360p', '240p', '144p'].map(() => true)});
-        yt.pipe(fs.createWriteStream('./' + VID + '.mp4'));
-
-        yt.on('end', async () => {
-            reply = await message.client.sendMessage(message.jid,YTV_UP,MessageType.text , {quoted: message.data});
-            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4 , quoted: message.data});
-        });
-    } else {
-
-
-    const linkk = match[1]
-    if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
-    await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text , {quoted: message.data});
-    await axios
-      .get(`https://rei-api.herokuapp.com/api/dl/ytavv2?url=${linkk}`)
-      .then(async (response) => {
-        const {
-          link,
-        } = response.data.result
-        const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
-        await message.client.sendMessage(message.jid,YSTV_UP,MessageType.text , {quoted: message.data});
-        await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {quoted: message.data ,mimetype: Mimetype.mp4, ptt: false})
-    })
-    .catch(
-      async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
-    )
+        await axios
+        .get(`https://rei-api.herokuapp.com/api/dl/ytavv2?url=https://www.youtube.com/watch?v=`+VID)
+        .then(async (response) => {
+          const {
+            link,
+          } = response.data.result
+          const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
+          await message.client.sendMessage(message.jid,YSTV_UP,MessageType.text , {quoted: message.data});
+          await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {quoted: message.data ,mimetype: Mimetype.mp4, ptt: false})
+        })
+        
+}))
 
 
+// public mode for me
 
+lusifar.addCommand({
+	
+	            pattern: 'video ?(.*)', 
+		    fromMe: PUBFM , 
+	            desc: YTV_DESC
 
-    }
+                                       }, (async (message, match) => { 
 
-}));
-
-
-
-//in public for me 
-
- lusifar.addCommand({pattern: 'video ?(.*)', fromMe: PUBH, desc: YTV_DESC}, (async (message, match) => { 
-
-        if (match[1] === '') return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text , {quoted: message.data});    
-        if (match[1].includes( 'shorts' )){
-        var VID = '';
-        try {
-            if (match[1].includes('watch')) {
+await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text , {quoted: message.data});   
+    
+    var VID = '';
+    try {
+            if (match[1].includes( 'shorts' )){
+            var rmx = match[1].replace( 'shorts/', '')
+            var rmy = rmx.replace( '?feature=share','')
+            var data = rmy.split( '/' )[3]
+            VID = data
+								
+         } else
+         
+         
+                 if (match[1].includes('watch')) {
                 var tsts = match[1].replace('watch?v=', '')
                 var alal = tsts.split('/')[3]
                 VID = alal
             } 
-            
-if (match[1].includes( 'shorts' )) {
-                var rmx = match[1].replace( 'shorts/', '')
-				var rmy = rmx.replace( '?feature=share','')
-                var data = rmy.split( '/' )[3]
-                VID = data
-            
-             }
-            
-            else {     
-                VID = match[1].split('/')[3]
+         
+       else {     
+				 VID = match[1].split('/')[3]
             }
         } catch {
-            return await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text , {quoted: message.data});
+            return await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text);
         }
-        var reply = await message.client.sendMessage(message.jid,DSWLOAD_VID,MessageType.text , {quoted: message.data});
+        await axios
+        .get(`https://rei-api.herokuapp.com/api/dl/ytavv2?url=https://www.youtube.com/watch?v=`+VID)
+        .then(async (response) => {
+          const {
+            link,
+          } = response.data.result
+          const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
+          await message.client.sendMessage(message.jid,YSTV_UP,MessageType.text , {quoted: message.data});
+          await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {quoted: message.data ,mimetype: Mimetype.mp4, ptt: false})
+        })
+        
+}))
 
-        var yt = ytdl(VID, {filter: format => format.container === 'mp4' && ['720p', '480p', '360p', '240p', '144p'].map(() => true)});
-        yt.pipe(fs.createWriteStream('./' + VID + '.mp4'));
-
-        yt.on('end', async () => {
-            reply = await message.client.sendMessage(message.jid,YTV_UP,MessageType.text , {quoted: message.data});
-            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4 , quoted: message.data});
-        });
-    } else {
-
-
-    const linkk = match[1]
-    if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
-    await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text , {quoted: message.data});
-    await axios
-      .get(`https://rei-api.herokuapp.com/api/dl/ytavv2?url=${linkk}`)
-      .then(async (response) => {
-        const {
-          link,
-        } = response.data.result
-        const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
-        await message.client.sendMessage(message.jid,YSTV_UP,MessageType.text , {quoted: message.data});
-        await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {quoted: message.data ,mimetype: Mimetype.mp4, ptt: false})
-    })
-    .catch(
-      async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
-    )
-
-
-
-
-    }
-
-}));
